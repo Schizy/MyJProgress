b22f340339d4c71cccca10e7355cdb4af922fc4d
Schizy <ali.mechichi@gmail.com>
2020-11-10 15:45:59 +0100

    Adds Bootstrap 4 blog template

---
+++ symfony/src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+// src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function number(): Response
+    {
+        return $this->render('base.html.twig', [
+            'title' => 'ようこそ！',
+        ]);
+    }
+}
