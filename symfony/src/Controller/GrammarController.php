20266a29a0e7220551ef71ffc15bbc07aab6a65d
Schizy <ali.mechichi@gmail.com>
2020-12-07 15:23:30 +0100

    Adds Create Grammar Route

--- symfony/src/Controller/GrammarController.php
+++ symfony/src/Controller/GrammarController.php
@@ -3,16 +3,31 @@
 // src/Controller/HomeController.php
 namespace App\Controller;
 
+use App\Entity\Example;
 use App\Entity\Grammar;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/grammars/")
+ */
 class GrammarController extends AbstractController
 {
     /**
-     * @Route("/grammars/{id}-{rule}", name="grammar-rule")
+     * @Route("", name="grammar-list")
+     */
+    public function list(EntityManagerInterface $em)
+    {
+        return $this->render('grammar/list.html.twig', [
+            'title' => 'Liste des règles de grammaire',
+            'grammars' => $em->getRepository(Grammar::class)->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("{id}-{rule}", name="grammar-rule")
      */
     public function rule(Grammar $grammar): Response
     {
@@ -23,12 +38,34 @@ class GrammarController extends AbstractController
     }
 
     /**
-     * @Route("/grammars", name="grammar-list")
+     * @Route("add", name="grammar-add")
      */
-    public function list(EntityManagerInterface $em)
+    public function add(EntityManagerInterface $em)
     {
-        return $this->render('grammar/list.html.twig', [
-            'title' => 'Liste des règles de grammaire',
+//        $grammar = new Grammar();
+//        $grammar
+//            ->setName('ば〜ほど')
+//            ->addExample(
+//                (new Example())
+//                    ->setPhrase('よめば　よむほど　簡単になる。')
+//                    ->setTranslation('Plus tu lis plus ça devient facile.')
+//                    ->setGrammar($grammar)
+//            )->addExample(
+//                (new Example())
+//                    ->setPhrase('よめば　よむほど　簡単になる。2')
+//                    ->setTranslation('Plus tu lis plus ça devient facile. 2')
+//                    ->setGrammar($grammar)
+//            )->addExample(
+//                (new Example())
+//                    ->setPhrase('よめば　よむほど　簡単になる。3')
+//                    ->setTranslation('Plus tu lis plus ça devient facile. 3')
+//                    ->setGrammar($grammar)
+//            );
+//        $em->persist($grammar);
+//        $em->flush();
+
+        return $this->render('grammar/add.html.twig', [
+            'title' => 'Nouvelle grammaire',
             'grammars' => $em->getRepository(Grammar::class)->findAll(),
         ]);
     }
--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -17,33 +17,9 @@ class HomeController extends AbstractController
      */
     public function home(EntityManagerInterface $em): Response
     {
-        $grammar = new Grammar();
-        $grammar
-            ->setName('ば〜ほど')
-            ->addExample(
-                (new Example())
-                    ->setPhrase('よめば　よむほど　簡単になる。')
-                    ->setTranslation('Plus tu lis plus ça devient facile.')
-                    ->setGrammar($grammar)
-            )->addExample(
-                (new Example())
-                    ->setPhrase('よめば　よむほど　簡単になる。2')
-                    ->setTranslation('Plus tu lis plus ça devient facile. 2')
-                    ->setGrammar($grammar)
-            )->addExample(
-                (new Example())
-                    ->setPhrase('よめば　よむほど　簡単になる。3')
-                    ->setTranslation('Plus tu lis plus ça devient facile. 3')
-                    ->setGrammar($grammar)
-            );
-        $em->persist($grammar);
-        $em->flush();
-
-        $rules = $em->getRepository(Grammar::class)->findBy([], ['id' => 'desc'], 5);
-
         return $this->render('home/home.html.twig', [
             'title' => 'ようこそ！',
-            'rules' => $rules,
+            'rules' => $em->getRepository(Grammar::class)->findBy([], ['id' => 'desc'], 5),
         ]);
     }
 }
