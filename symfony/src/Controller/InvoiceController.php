a847a5495977c9ec6e7b24380e711b65eb7b6616
Schizy <ali.mechichi@gmail.com>
2021-03-23 22:00:26 +0100

    Better PDF invoice

--- symfony/src/Controller/InvoiceController.php
+++ symfony/src/Controller/InvoiceController.php
@@ -10,7 +10,45 @@ use Symfony\Component\Routing\Annotation\Route;
 class InvoiceController extends AbstractController
 {
     #[Route('/invoice', name: 'invoice')]
-    public function index(): Response
+    public function invoice(): Response
+    {
+        $view =  $this->renderView('invoice/invoice.html.twig', [
+            'my' => [
+                'name' => $_ENV['MY_NAME'],
+                'address' => $_ENV['MY_ADDRESS'],
+                'city' => $_ENV['MY_CITY'],
+                'siret' => $_ENV['MY_SIRET'],
+                'tva' => $_ENV['MY_TVA'],
+                'bank' => $_ENV['MY_BANK'],
+            ],
+            'client' => [
+                'name' => $_ENV['CLIENT_NAME'],
+                'address' => $_ENV['CLIENT_ADDRESS'],
+                'city' => $_ENV['CLIENT_CITY'],
+                'siret' => $_ENV['CLIENT_SIRET'],
+            ],
+        ]);
+        die($view);
+
+
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($view);
+
+        // (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A4');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser
+        $dompdf->stream();
+        exit;
+    }
+
+
+    #[Route('/invoice/pdf', name: 'invoicePdf')]
+    public function invoicePdf(): Response
     {
         $view =  $this->renderView('invoice/invoice.html.twig', [
             'my' => [
@@ -35,7 +73,7 @@ class InvoiceController extends AbstractController
         $dompdf->loadHtml($view);
 
         // (Optional) Setup the paper size and orientation
-//        $dompdf->setPaper('A4');
+        $dompdf->setPaper('A4');
 
         // Render the HTML as PDF
         $dompdf->render();
